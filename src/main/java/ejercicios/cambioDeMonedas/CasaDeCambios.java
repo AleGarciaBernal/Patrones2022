6819304c14787cf9df26499f851b3530d3e45c86
AleGarciaBernal <aleandy7@gmail.com>
2022-05-19 23:23:54 -0400

    Primer Ejercicio

---
+++ src/main/java/ejercicios/cambioDeMonedas/CasaDeCambios.java
@@ -0,0 +1,26 @@
+package ejercicios.cambioDeMonedas;
+
+public class CasaDeCambios {
+    private String nombre;
+
+    public CasaDeCambios(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void cambiarBsADolares(int cantidad){
+        ManagerCambio.getInstance().bsADolares(cantidad);
+
+    }
+    public void cambiarBsAEuros(int cantidad){
+        ManagerCambio.getInstance().bsAEuros(cantidad);
+
+    }
+    public void cambiarDolaresABs(int cantidad){
+        ManagerCambio.getInstance().dolarABs(cantidad);
+
+    }
+    public void cambiarEurosABs(int cantidad){
+        ManagerCambio.getInstance().euroABs(cantidad);
+
+    }
+}
